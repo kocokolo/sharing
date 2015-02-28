ab32e5dac67774b643a091a64541921c0109aea3
cui.peng <cuipeng02@baidu.com>
2015-02-28 18:53:54 +0800

    u

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 (function () {
     var port = 8010,
-        http = require('http');
-
-    var express = require('express'),
+        http = require('http'),
+        express = require('express'),
         app = express(),
         server = http.createServer(app);
 
     var io = require('socket.io'),
         sio = io.listen(server);
 
+    // 启动webserver静态资源访问
     require('./server/httprequest.js').init(app, __dirname);
+    // 启动demo painter的通信请求支持
     require('./server/painter.js').init(sio);
+    // 启动demo screensharer的通信请求支持
     require('./server/screensharer.js').init(sio);
 
     server.listen(port);
-
     console.log("app start success");
-
 }).call(this);
--- server/httprequest.js
+++ server/httprequest.js
@@ -1,9 +1,15 @@
+/**
+ * 基本的webserver功能，提供对静态资源文件的访问，如html,js,css,image
+ * 提供本次展示的PPT和demo的访问
+ */
 function initWebserver(app,root) {
 
+    // 根目录返回 index.html 文件
     app.get('/', function (req, res) {
         res.sendfile(root + '/index.html');
     });
 
+    // 基于请求的文件地址，返回对应的服务器上的文件
     app.get('/*', function (req, res, next) {
         var file = req.params[0];
         res.sendfile(root + '/' + file);
